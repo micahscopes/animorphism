02661e55255ecada55a5059a6844289365ba51e4
Micah(phones) <fitchmicah@gmail.com>
2015-11-01 10:28:27 -0600

    added default export

--- animorphism.js
+++ animorphism.js
@@ -1,4 +1,4 @@
-export function Animorphism(there,back){
+export default function Animorphism(there,back){
   this.do = (f) => (source) => back(f(there(source)),source);
   this.reverse = function(){return new Animorphism(back,there)} // hypothetically
 }
